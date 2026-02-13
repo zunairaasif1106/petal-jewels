fb49df361719b3621f8780369c1c08c7cafaf9a8
Zunera Asif <zunairaasif1106@gmail.com>
2026-02-13 21:42:34 +0500

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,13 @@
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBkBtLjZi1BGBBMdg-qX2MM4F2H7HeaHs4",
-  databaseURL: "https://petal-gems-default-rtdb.firebaseio.com",
-  authDomain: "petal-gems.firebaseapp.com",
-  projectId: "petal-gems",
-  storageBucket: "petal-gems.firebasestorage.app",
-  messagingSenderId: "342994581723",
-  appId: "1:342994581723:web:db898df3b2d499939374fe",
-  measurementId: "G-0LE0NB92CY"
+  apiKey: "AIzaSyAk7dr-HWZcsGWIinUj-mLN9UR_uoR74BU",
+  authDomain: "petal-jewels.firebaseapp.com",
+  databaseURL: "https://petal-jewels-default-rtdb.firebaseio.com",
+  projectId: "petal-jewels",
+  storageBucket: "petal-jewels.firebasestorage.app",
+  messagingSenderId: "401348304080",
+  appId: "1:401348304080:web:06ba6a652ba097cb3512d5",
+  measurementId: "G-QW0HNJ4FJX"
 };
 
 // Initialize Firebase
